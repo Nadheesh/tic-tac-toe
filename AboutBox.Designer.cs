a058225eeb91f9a48ad2cc97ef9bd142ed7c7cec
mithwick93 <mithwick93@gmail.com>
2015-08-01 20:53:25 +0530

    resx

---
+++ AboutBox.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace UI {
+	partial class AboutBox {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.picBox = new System.Windows.Forms.PictureBox();
+			this.labelProductName = new System.Windows.Forms.Label();
+			this.labelVersion = new System.Windows.Forms.Label();
+			this.labelCompanyName = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnOK = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// picBox
+			// 
+			this.picBox.Image = global::TicTacToe.Properties.Resources._256x256;
+			this.picBox.Location = new System.Drawing.Point(63, 12);
+			this.picBox.Name = "picBox";
+			this.picBox.Size = new System.Drawing.Size(269, 232);
+			this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.picBox.TabIndex = 0;
+			this.picBox.TabStop = false;
+			// 
+			// labelProductName
+			// 
+			this.labelProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelProductName.Location = new System.Drawing.Point(58, 256);
+			this.labelProductName.Name = "labelProductName";
+			this.labelProductName.Size = new System.Drawing.Size(274, 43);
+			this.labelProductName.TabIndex = 1;
+			this.labelProductName.Text = "Product name";
+			this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// labelVersion
+			// 
+			this.labelVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelVersion.Location = new System.Drawing.Point(58, 299);
+			this.labelVersion.Name = "labelVersion";
+			this.labelVersion.Size = new System.Drawing.Size(274, 43);
+			this.labelVersion.TabIndex = 2;
+			this.labelVersion.Text = "Version ";
+			this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// labelCompanyName
+			// 
+			this.labelCompanyName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelCompanyName.Location = new System.Drawing.Point(58, 342);
+			this.labelCompanyName.Name = "labelCompanyName";
+			this.labelCompanyName.Size = new System.Drawing.Size(274, 43);
+			this.labelCompanyName.TabIndex = 3;
+			this.labelCompanyName.Text = "Company name";
+			this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(59, 394);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(273, 23);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "130556L - Yasas Senarath";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(59, 427);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(273, 23);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "130650U - M.S.Wickramarathne";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// btnOK
+			// 
+			this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnOK.Location = new System.Drawing.Point(318, 459);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 6;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// AboutBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btnOK;
+			this.ClientSize = new System.Drawing.Size(405, 494);
+			this.Controls.Add(this.btnOK);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.labelCompanyName);
+			this.Controls.Add(this.labelVersion);
+			this.Controls.Add(this.labelProductName);
+			this.Controls.Add(this.picBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "AboutBox";
+			this.Padding = new System.Windows.Forms.Padding(9);
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "About Tic Tac Toe";
+			((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox picBox;
+		private System.Windows.Forms.Label labelProductName;
+		private System.Windows.Forms.Label labelVersion;
+		private System.Windows.Forms.Label labelCompanyName;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnOK;
+
+	}
+}
